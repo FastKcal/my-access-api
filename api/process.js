44cf22dba0762fd19991289fd321609c8cea887a
FastKcal <szybkawiewiorka@int.pl>
2026-01-27 18:35:52 +0100

    Update process.js

--- api/process.js
+++ api/process.js
@@ -7,55 +7,25 @@ export default async function handler(req, res) {
         return res.status(200).end();
     }
     
-    // GET /api/process - zwraca to samo co POST (dla kompatybilności)
-    if (req.method === 'GET') {
-        try {
-            const part1 = Math.floor(Math.random() * 1000000000).toString();
-            const part2 = Math.floor(Math.random() * 1000000000).toString();
-            const part3 = Math.floor(Math.random() * 1000000000).toString();
-            const queryString = `8099_${part1}${part2}${part3}`;
-            
-            const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-            let token = '';
-            for (let i = 0; i < 74; i++) {
-                token += chars.charAt(Math.floor(Math.random() * chars.length));
-            }
-            
-            return res.status(200).json([
-                'frankfurt',
-                '1',
-                queryString,
-                token
-            ]);
-        } catch (error) {
-            return res.status(500).json({ error: error.message });
+    try {
+        // Query format: 8099_5229009146648096195797666599 (28 cyfr po _)
+        const randomDigits = Math.floor(Math.random() * 1e28).toString().padStart(28, '0');
+        const queryString = `8099_${randomDigits}`;
+        
+        // Token 74 chars
+        const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        let token = '';
+        for (let i = 0; i < 74; i++) {
+            token += chars.charAt(Math.floor(Math.random() * chars.length));
         }
+        
+        return res.status(200).json([
+            'frankfurt',
+            '5',
+            queryString,
+            token
+        ]);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-    
-    // POST /api/process
-    if (req.method === 'POST') {
-        try {
-            const part1 = Math.floor(Math.random() * 1000000000).toString();
-            const part2 = Math.floor(Math.random() * 1000000000).toString();
-            const part3 = Math.floor(Math.random() * 1000000000).toString();
-            const queryString = `8099_${part1}${part2}${part3}`;
-            
-            const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-            let token = '';
-            for (let i = 0; i < 74; i++) {
-                token += chars.charAt(Math.floor(Math.random() * chars.length));
-            }
-            
-            return res.status(200).json([
-                'frankfurt',
-                '1',
-                queryString,
-                token
-            ]);
-        } catch (error) {
-            return res.status(500).json({ error: error.message });
-        }
-    }
-    
-    return res.status(405).json({ error: 'Method not allowed' });
 }
